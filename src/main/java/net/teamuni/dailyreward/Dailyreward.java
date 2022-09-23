b68e145f1a471db4023312fe4d77aa22105ea076
Fade <jjw4016@gmail.com>
2022-09-23 20:24:23 +0900

    플러그인 기본 설정

---
+++ src/main/java/net/teamuni/dailyreward/Dailyreward.java
@@ -0,0 +1,17 @@
+package net.teamuni.dailyreward;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Dailyreward extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
