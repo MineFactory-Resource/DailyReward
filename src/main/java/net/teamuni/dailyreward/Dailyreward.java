e229c5f083f8306bf0a28a848e026a94aedd155f
Fade <jjw4016@gmail.com>
2022-09-24 19:37:12 +0900

    /출석체크 명령어 입력시 GUI 열리게 하기

--- src/main/java/net/teamuni/dailyreward/Dailyreward.java
+++ src/main/java/net/teamuni/dailyreward/Dailyreward.java
@@ -1,17 +1,36 @@
 package net.teamuni.dailyreward;
 
+import org.bukkit.*;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import org.bukkit.event.Listener;
+import org.bukkit.inventory.Inventory;
+import org.bukkit.inventory.ItemStack;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.jetbrains.annotations.NotNull;
 
-public final class Dailyreward extends JavaPlugin {
+public final class Dailyreward extends JavaPlugin implements Listener {
+
 
     @Override
     public void onEnable() {
-        // Plugin startup logic
-
     }
 
     @Override
     public void onDisable() {
         // Plugin shutdown logic
+    }
+    @Override
+    public boolean onCommand(@NotNull CommandSender sender, Command cmd, @NotNull String label, String[] args) {
+        Player player = (Player) sender;
+        if (cmd.getName().equals("출석체크") && player.hasPermission("dailyreward.opengui")) {
+            Inventory DailyRewardGui = Bukkit.createInventory(player, 54, ChatColor.GREEN + "출석체크 GUI");
+            ItemStack day1 = new ItemStack(Material.CHEST_MINECART);
+            ItemStack[] DailyRewardItems = {day1};
+            DailyRewardGui.setContents(DailyRewardItems);
+            player.openInventory(DailyRewardGui);
+        }
+        return true;
     }
 }
