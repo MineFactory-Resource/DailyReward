5e09889c42adf511089c2853c69dea041efedd9c
Fade <jjw4016@gmail.com>
2022-09-28 19:47:33 +0900

    ClickEvent 클래스에 InventoryDragEvent도 캔슬되게끔 추가

--- src/main/java/net/teamuni/dailyreward/ClickEvent.java
+++ src/main/java/net/teamuni/dailyreward/ClickEvent.java
@@ -3,10 +3,16 @@ package net.teamuni.dailyreward;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.inventory.InventoryClickEvent;
+import org.bukkit.event.inventory.InventoryDragEvent;
 
 public class ClickEvent implements Listener {
     @EventHandler
     public void clickEvent(InventoryClickEvent e) {
+        e.setCancelled(true);
+    }
+
+    @EventHandler
+    public void clickEvent(InventoryDragEvent e) {
         e.setCancelled(true);
     }
 }
