8e2c85a3c391ba8848461a78f5802cb0a53d4001
Fade <jjw4016@gmail.com>
2022-09-29 19:35:24 +0900

    플러그인이 활성화 될 때만 GUI의 정보 담게끔 하기

--- src/main/java/net/teamuni/dailyreward/Dailyreward.java
+++ src/main/java/net/teamuni/dailyreward/Dailyreward.java
@@ -1,47 +1,34 @@
 package net.teamuni.dailyreward;
 
-import org.bukkit.*;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
-import org.bukkit.entity.Item;
 import org.bukkit.entity.Player;
 import org.bukkit.event.Listener;
-import org.bukkit.inventory.Inventory;
-import org.bukkit.inventory.ItemStack;
-import org.bukkit.inventory.meta.ItemMeta;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.jetbrains.annotations.NotNull;
 
-import java.util.*;
-
 public final class Dailyreward extends JavaPlugin implements Listener {
-    private final Map<Integer, ItemStack> dailyItem = new HashMap<>();
-    private final Set<ItemMeta> dailyItemMetaSet = new HashSet<>();
 
     @Override
     public void onEnable() {
         getServer().getPluginManager().registerEvents(new ClickEvent(), this);
-        RewardManager.createRewardsYml();
+        RewardManager rm = new RewardManager();
+        rm.createRewardsYml();
+        rm.setGui();
     }
 
     @Override
     public void onDisable() {
-        // Plugin shutdown logic
+        RewardManager rm = new RewardManager();
+        rm.save();
     }
+
 
     @Override
     public boolean onCommand(@NotNull CommandSender sender, Command cmd, @NotNull String label, String[] args) {
         Player player = (Player) sender;
         if (cmd.getName().equals("출석체크") && player.hasPermission("dailyreward.opengui")) {
-            dailyItem.putAll(RewardManager.getRewards("Rewards"));
-            for (ItemStack itemStack : dailyItem.values()) {
-                this.dailyItemMetaSet.add(itemStack.getItemMeta());
-            }
-            Inventory DailyRewardGui = Bukkit.createInventory(player, 54, ChatColor.GREEN + "출석체크 GUI");
-            for (Map.Entry<Integer, ItemStack> dailyitems : dailyItem.entrySet()){
-                DailyRewardGui.setItem(dailyitems.getKey(), dailyitems.getValue());
-            }
-            player.openInventory(DailyRewardGui);
+            player.openInventory(RewardManager.DailyRewardGui);
         }
         return true;
     }
