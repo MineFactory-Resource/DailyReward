fe0bb7eb4cdc38ba9c7b88e48b947afb4d9350ca
Fade <jjw4016@gmail.com>
2022-09-30 17:59:01 +0900

    DailyReward reload 기능 추가

--- src/main/java/net/teamuni/dailyreward/Dailyreward.java
+++ src/main/java/net/teamuni/dailyreward/Dailyreward.java
@@ -1,5 +1,6 @@
 package net.teamuni.dailyreward;
 
+import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
@@ -8,6 +9,7 @@ import org.bukkit.plugin.java.JavaPlugin;
 import org.jetbrains.annotations.NotNull;
 
 public final class Dailyreward extends JavaPlugin implements Listener {
+
 
     @Override
     public void onEnable() {
@@ -19,17 +21,29 @@ public final class Dailyreward extends JavaPlugin implements Listener {
 
     @Override
     public void onDisable() {
-        RewardManager rm = new RewardManager();
-        rm.reload();
-        rm.save();
     }
 
 
     @Override
     public boolean onCommand(@NotNull CommandSender sender, Command cmd, @NotNull String label, String[] args) {
+        RewardManager rm = new RewardManager();
         Player player = (Player) sender;
         if (cmd.getName().equals("출석체크") && player.hasPermission("dailyreward.opengui")) {
-            player.openInventory(RewardManager.DailyRewardGui);
+            player.openInventory(rm.DailyRewardGui);
+        }
+        if (cmd.getName().equals("dailyreward") && player.hasPermission("dailyreward.reload")) {
+            if (args.length > 0) {
+                if (args[0].equals("reload")) {
+                    rm.reload();
+                    rm.save();
+                    rm.setGui();
+                    player.sendMessage(ChatColor.YELLOW + "[알림]" + ChatColor.WHITE + " DailyReward 플러그인이 리로드되었습니다.");
+                    return true;
+                }
+            } else {
+                player.sendMessage(ChatColor.YELLOW + "[알림]" + ChatColor.WHITE + " 알 수 없는 명령어 입니다.");
+                return true;
+            }
         }
         return true;
     }
