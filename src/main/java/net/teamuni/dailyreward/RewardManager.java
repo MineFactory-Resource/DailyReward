7e106564e42e5e6daa6cc4d588ae55826e9b0d23
Fade <jjw4016@gmail.com>
2022-10-01 20:38:14 +0900

    rewards.yml 메세지 수정, String 연산 최소화

--- src/main/java/net/teamuni/dailyreward/RewardManager.java
+++ src/main/java/net/teamuni/dailyreward/RewardManager.java
@@ -3,6 +3,7 @@ package net.teamuni.dailyreward;
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.Material;
+import org.bukkit.configuration.ConfigurationSection;
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.event.Listener;
@@ -22,7 +23,7 @@ public class RewardManager implements Listener {
     private final Set<ItemMeta> dailyItemMetaSet = new HashSet<>();
     private File file = null;
     private FileConfiguration rewardsFile = null;
-    public final Inventory DailyRewardGui = Bukkit.createInventory(null, 54, ChatColor.GREEN + "출석체크 GUI");
+    public final Inventory dailyRewardGui = Bukkit.createInventory(null, 54, ChatColor.GREEN + "출석체크 GUI");
 
     public void createRewardsYml() {
         this.file = new File(main.getDataFolder(), "rewards.yml");
@@ -32,14 +33,11 @@ public class RewardManager implements Listener {
         }
         this.rewardsFile = YamlConfiguration.loadConfiguration(file);
     }
-    public FileConfiguration getConfig() {
-        return this.rewardsFile;
-    }
 
     public void reload() {
         this.rewardsFile = YamlConfiguration.loadConfiguration(file);
     }
-    
+
     /*
     public void save() {
         try{
@@ -53,30 +51,35 @@ public class RewardManager implements Listener {
     @NotNull
     public Map<Integer, ItemStack> getRewards(String path){
         Map<Integer, ItemStack> rewards = new HashMap<>();
-        Set<String> rewardsKeys = getConfig().getConfigurationSection(path).getKeys(false);
+        ConfigurationSection section = this.rewardsFile.getConfigurationSection(path);
+        Set<String> rewardsKeys = section.getKeys(false);
         if (rewardsKeys.isEmpty()) {
-            throw new IllegalArgumentException("rewards.yml 에서 문제가 발생했습니다.");
+            throw new IllegalArgumentException("rewards.yml 파일의 내용이 비어있습니다. rewards.yml파일을 확인해주세요.");
         }
         for (String key : rewardsKeys) {
-            int slot = getConfig().getInt(path + "." + key + ".slot");
+            int slot = section.getInt(key + ".slot");
             try {
-                ItemStack rewardsItem = new ItemStack(Material.valueOf(getConfig().getString(path + "." + key +".item_type")));
+                ItemStack rewardsItem = new ItemStack(Material.valueOf(section.getString(key + ".item_type")));
                 ItemMeta meta = rewardsItem.getItemMeta();
-                String rewardsName = getConfig().getString(path + "." + key + ".name");
+                String rewardsName = section.getString(key + ".name");
                 List<String> rewardLoreList = new ArrayList<>();
                 List<String> commandList = new ArrayList<>();
-                for (String lores : getConfig().getStringList(path + "." + key + ".lore")){
+                for (String lores : section.getStringList(key + ".lore")){
                     rewardLoreList.add(ChatColor.translateAlternateColorCodes('&', lores));
                 }
-                for (String commands : getConfig().getStringList(path + "." + key + ".commands")){
+                for (String commands : section.getStringList(key + ".commands")){
                     commandList.add(commands);
                 }
-                meta.setDisplayName(ChatColor.translateAlternateColorCodes('&', rewardsName));
+                if (rewardsName != null) {
+                    meta.setDisplayName(ChatColor.translateAlternateColorCodes('&', rewardsName));
+                } else {
+                    Bukkit.getLogger().info("rewards.yml 파일중 보상의 이름이 없습니다. rewards.yml을 확인해주세요.");
+                }
                 meta.setLore(rewardLoreList);
                 meta.addItemFlags(ItemFlag.HIDE_ATTRIBUTES);
                 rewardsItem.setItemMeta(meta);
                 rewards.put(slot, rewardsItem);
-            } catch (NullPointerException | IllegalArgumentException e) {
+            } catch (IllegalArgumentException e) {
                 e.printStackTrace();
             }
         }
@@ -89,7 +92,7 @@ public class RewardManager implements Listener {
             this.dailyItemMetaSet.add(itemStack.getItemMeta());
         }
         for (Map.Entry<Integer, ItemStack> dailyitems : this.dailyItem.entrySet()){
-            this.DailyRewardGui.setItem(dailyitems.getKey(), dailyitems.getValue());
+            this.dailyRewardGui.setItem(dailyitems.getKey(), dailyitems.getValue());
         }
     }
 }
