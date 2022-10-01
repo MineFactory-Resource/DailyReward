cbe92babb862671f607adbf5bfbfb39d2395f8a5
Fade <jjw4016@gmail.com>
2022-10-01 19:32:45 +0900

    클릭이벤트 클래스에 생성자 사용

--- src/main/java/net/teamuni/dailyreward/ClickEvent.java
+++ src/main/java/net/teamuni/dailyreward/ClickEvent.java
@@ -4,17 +4,23 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.inventory.InventoryClickEvent;
 import org.bukkit.event.inventory.InventoryDragEvent;
+import org.bukkit.inventory.Inventory;
 
 public class ClickEvent implements Listener {
+    private final Inventory inventory;
+    public ClickEvent(RewardManager rewardManager){
+        this.inventory = rewardManager.DailyRewardGui;
+    }
+
+
     @EventHandler
     public void clickEvent(InventoryClickEvent e) {
-        if (!e.getInventory().equals(RewardManager.DailyRewardGui)) return;
+        if(!e.getInventory().equals(inventory)) return;
         e.setCancelled(true);
-
     }
     @EventHandler
     public void dragEvent(InventoryDragEvent e) {
-        if (!e.getInventory().equals(RewardManager.DailyRewardGui)) return;
+        if(!e.getInventory().equals(inventory)) return;
         e.setCancelled(true);
     }
 }
