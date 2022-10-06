2fcefea62404ccf4300defb6852b6bf4fe0b7efe
Fade <jjw4016@gmail.com>
2022-10-06 19:49:01 +0900

    플레이어가 서버에 접속했을때, 자신의 UUID을 파일명으로 하는 yml파일 생성되게끔 하기.

--- src/main/java/net/teamuni/dailyreward/Event.java
+++ src/main/java/net/teamuni/dailyreward/Event.java
@@ -7,22 +7,37 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.inventory.InventoryClickEvent;
 import org.bukkit.event.inventory.InventoryDragEvent;
+import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.inventory.Inventory;
 
 import java.io.File;
+import java.io.IOException;
 import java.util.List;
 import java.util.Set;
 
-public class ClickEvent implements Listener {
+public class Event implements Listener {
     private final Inventory inventory;
 
-    public ClickEvent(RewardManager rewardManager){ this.inventory = rewardManager.dailyRewardGui; }
+    public Event(RewardManager rewardManager){ this.inventory = rewardManager.dailyRewardGui; }
 
     public ConfigurationSection loadConfiguration() {
         File file = new File("plugins/Dailyreward", "rewards.yml");
         YamlConfiguration yaml = YamlConfiguration.loadConfiguration(file);
         return yaml.getConfigurationSection("Rewards");
     }
+    @EventHandler
+    public void joinEvent(PlayerJoinEvent e){
+        File file = new File("plugins/Dailyreward/Players",e.getPlayer().getUniqueId()+".yml");
+        if (!file.exists()) {
+            try {
+                file.createNewFile();
+            } catch (Exception exception){
+                exception.printStackTrace();
+            }
+        }
+    }
+
+
 
 
     @EventHandler
