2928ffda6945704df0c1334ece3cf1e4cf20ffd5
Fade <jjw4016@gmail.com>
2022-10-10 20:20:12 +0900

    사용안하는 import, 구문 삭제

--- src/main/java/net/teamuni/dailyreward/Dailyreward.java
+++ src/main/java/net/teamuni/dailyreward/Dailyreward.java
@@ -4,14 +4,12 @@ import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.configuration.file.FileConfiguration;
-import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.entity.Player;
 import org.bukkit.event.Listener;
 import org.bukkit.inventory.Inventory;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.jetbrains.annotations.NotNull;
 
-import java.io.File;
 
 public final class Dailyreward extends JavaPlugin implements Listener {
     private RewardManager rewardManager;
@@ -31,10 +29,6 @@ public final class Dailyreward extends JavaPlugin implements Listener {
 
     @Override
     public void onDisable() {
-    }
-
-    public FileConfiguration rewardsYmlLoad() {
-        return rewardManager.getrewardsFile();
     }
 
 
