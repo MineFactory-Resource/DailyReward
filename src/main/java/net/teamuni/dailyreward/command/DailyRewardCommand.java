a058785c04548fbc10d2e6f4b35c5e38ad89639a
Fade <jjw4016@gmail.com>
2022-11-13 00:53:52 +0900

    불필요한 import 제거

--- src/main/java/net/teamuni/dailyreward/command/DailyRewardCommand.java
+++ src/main/java/net/teamuni/dailyreward/command/DailyRewardCommand.java
@@ -1,7 +1,6 @@
 package net.teamuni.dailyreward.command;
 
 import net.teamuni.dailyreward.DailyReward;
-import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
